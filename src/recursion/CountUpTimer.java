087071bafc71489f08e24ce42cf2bea9b7dfb0af
Xingyvo <159099828+Xingyvo@users.noreply.github.com>
2024-11-25 18:40:48 -0500

    Completed the first question.

--- src/recursion/CountUpTimer.java
+++ src/recursion/CountUpTimer.java
@@ -2,7 +2,15 @@ package recursion;
 
 public class CountUpTimer {
     public static void countUp(int n) {
-        // solution here
+        // solution here {
+        if (n <= 0) {
+            // Base case: stop when countdown reaches 0
+            System.out.println("Countdown start!");
+        } else {
+            // Recursive case: call the method and then print the number
+            countUp(n - 1);
+            System.out.println(n);
+        }
     }
 
     public static void main(String[] args) {
