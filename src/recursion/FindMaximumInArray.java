3cdfdad8957e1f72364dc03af44b48aca546f4ac
Xingyvo <159099828+Xingyvo@users.noreply.github.com>
2024-11-26 12:29:26 -0500

    Worked on question #10

--- src/recursion/FindMaximumInArray.java
+++ src/recursion/FindMaximumInArray.java
@@ -2,19 +2,22 @@ package recursion;
 
 public class FindMaximumInArray {
     public static int findMax(int[] arr, int n) {
+        // Base case:
         if (n <= 0) {
             return 0;
         }
 
-        if (arr[n] > arr[n - 1]) {
-            return arr[n];
+        // Determine the largest integer in the array
+        if (arr[n - 1] > arr[n - 2]) {
+            return arr[n - 1];
         }
 
+        // Recursive case:
         return findMax(arr, n - 1); 
     }
 
     public static void main(String[] args) {
-        int[] arr = {2, 3, 9, 6};
-        System.out.println(findMax(arr, 4)); // Output: 9
+        int[] arr = {2, 3, 9, 6, 10, 12};
+        System.out.println(findMax(arr, 6)); // Output: 12
     }
 }
