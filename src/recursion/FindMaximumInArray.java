129ee4e4ccf62719885ab60aaf1ce259771e46f0
Xingyvo <159099828+Xingyvo@users.noreply.github.com>
2024-11-26 23:47:24 -0500

    Solved question #10 with the Math.max method.

--- src/recursion/FindMaximumInArray.java
+++ src/recursion/FindMaximumInArray.java
@@ -2,22 +2,17 @@ package recursion;
 
 public class FindMaximumInArray {
     public static int findMax(int[] arr, int n) {
-        // Base case:
-        if (n <= 0) {
-            return 0;
+        // Base case: if the array has only one element
+        if (n == 1) {
+            return arr[0];
         }
 
-        // Determine the largest integer in the array
-        if (arr[n - 1] > arr[n - 2]) {
-            return arr[n - 1];
-        }
-
-        // Recursive case:
-        return findMax(arr, n - 1); 
+        // Recursive case: find the maximum between the last element and the maximum of the rest of the array
+        return Math.max(arr[n - 1], findMax(arr, n - 1));
     }
 
     public static void main(String[] args) {
         int[] arr = {2, 3, 9, 6, 10, 12};
-        System.out.println(findMax(arr, 6)); // Output: 12
+        System.out.println(findMax(arr, arr.length)); // Output: 12
     }
 }
